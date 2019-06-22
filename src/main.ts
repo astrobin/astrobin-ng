57707a314024f5b21cfba4b8a9c120889783d63d
Salvatore Iovene <salvatore@iovene.com>
2019-06-22 09:20:47 +0200

    Add Swagger support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import {NestFactory} from "@nestjs/core";
 import {AppModule} from "./app.module";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    const options = new DocumentBuilder()
+        .setTitle("AstroBin Certified Equipment API")
+        .setDescription("The AstroBin Certified Equipment API description")
+        .setVersion("0.1")
+        .addTag("astrobin-certified-equipment")
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup("api", app, document);
+
     await app.listen(3000);
 }
 
