f5865561e41d041c2f44cb4b77d5c2a5ba9f0769
Salvatore Iovene <salvatore@iovene.com>
2019-06-22 21:15:44 +0200

    Remove unneeded providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import {Module} from "@nestjs/common";
 import {ConfigModule} from "./config.module";
 import {CertifiedEquipmentModule} from "./certified-equipment/certified-equipment.module";
-import {CertifiedEquipmentService} from "./certified-equipment/certified-equipment.service";
 import {TypeOrmModule} from "@nestjs/typeorm";
-import {Repository} from "typeorm";
 import {ConfigService} from "./config.service";
 
 @Module({
@@ -11,10 +9,6 @@ import {ConfigService} from "./config.service";
         TypeOrmModule.forRoot(ConfigService.getTypeOrmConfiguration()),
         ConfigModule,
         CertifiedEquipmentModule,
-    ],
-    providers: [
-        Repository,
-        CertifiedEquipmentService,
     ],
 })
 export class AppModule {
