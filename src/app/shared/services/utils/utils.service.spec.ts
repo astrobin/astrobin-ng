75b79d7fa194475ba9fa21507ab44428a96c1571
Salvatore Iovene <salvatore@iovene.com>
2020-08-02 16:59:41 +0200

    (uploader) Add check for file extension for IE < 10
    IE < 10 does not support the `accept` property on the file input.

--- src/app/shared/services/utils/utils.service.spec.ts
+++ src/app/shared/services/utils/utils.service.spec.ts
@@ -13,4 +13,17 @@ describe("UtilsService", () => {
   it("should be created", () => {
     expect(service).toBeTruthy();
   });
+
+  describe("fileExtension", () => {
+    it("should work", () => {
+      expect(service.fileExtension("foo.txt")).toEqual("txt");
+      expect(service.fileExtension("foo bar.txt")).toEqual("txt");
+      expect(service.fileExtension("名称.txt")).toEqual("txt");
+      expect(service.fileExtension("foo.bar.txt")).toEqual("txt");
+      expect(service.fileExtension("foo")).toBeUndefined();
+      expect(service.fileExtension("")).toBeUndefined();
+      expect(service.fileExtension(null)).toBeUndefined();
+      expect(service.fileExtension(undefined)).toBeUndefined();
+    });
+  });
 });
