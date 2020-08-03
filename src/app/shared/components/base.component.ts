a14db5b6fc8bedffe2e09f48e0fd99ff378a0274
Salvatore Iovene <salvatore@iovene.com>
2020-08-03 16:28:46 +0200

    (add decorator to base component)

--- src/app/shared/components/base.component.ts
+++ src/app/shared/components/base.component.ts
@@ -1,6 +1,8 @@
-import { OnDestroy } from "@angular/core";
+import { Directive, OnDestroy } from "@angular/core";
 import { Subject } from "rxjs";
 
+@Directive()
+// tslint:disable-next-line:directive-class-suffix
 export class BaseComponent implements OnDestroy {
   destroyedSubject = new Subject();
   destroyed$ = this.destroyedSubject.asObservable();
