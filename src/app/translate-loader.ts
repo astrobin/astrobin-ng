2b6c144b23a621a7ea9e78cbd8d5d8abbebe9c2f
Salvatore Iovene <salvatore@iovene.com>
2020-09-19 23:56:52 +0200

    (i18n) Fallback to en if language not found, fill missing vaules with keys (#311)

--- src/app/translate-loader.ts
+++ src/app/translate-loader.ts
@@ -4,26 +4,54 @@ import { environment } from "@env/environment";
 import { TranslatePoHttpLoader } from "@fjnr/ngx-translate-po-http-loader";
 import { TranslateLoader } from "@ngx-translate/core";
 import { JsonApiService } from "@shared/services/api/classic/json/json-api.service";
-import { forkJoin, Observable, of } from "rxjs";
+import { forkJoin, Observable } from "rxjs";
 import { catchError, map, switchMap } from "rxjs/operators";
 
 declare const VERSION: string;
 
 @Injectable()
 export class LanguageLoader extends TranslatePoHttpLoader implements TranslateLoader {
-  constructor(protected _http: HttpClient, private _jsonApi: JsonApiService) {
-    super(_http);
+  ngTranslations$ = (lang: string): Observable<object> => this.http.get(`/assets/i18n/${lang}.json?version=${VERSION}`);
+
+  classicTranslations$ = (lang: string): Observable<object> =>
+    this.jsonApi.getBackendConfig$().pipe(
+      switchMap(backendConfig =>
+        this.http
+          .get(`${environment.classicApiUrl}/json-api/i18n/messages/${lang}/?version=${backendConfig.i18nHash}`, {
+            responseType: "text"
+          })
+          .pipe(map((contents: string) => this.parse(contents)))
+      )
+    );
+
+  constructor(public http: HttpClient, public jsonApi: JsonApiService) {
+    super(http);
   }
 
   getTranslation(lang: string): Observable<any> {
     return forkJoin([
-      this._classicTranslations$(lang),
-      this._ngTranslations$(lang).pipe(catchError(() => of(null)))
+      this.classicTranslations$(lang),
+      this.ngTranslations$(lang).pipe(catchError(() => this.ngTranslations$("en")))
     ]).pipe(
       map(results => {
+        const classicTranslations = results[0];
+        const ngTranslations = results[1];
+
+        Object.keys(classicTranslations).forEach(key => {
+          if (classicTranslations[key] === "") {
+            classicTranslations[key] = key;
+          }
+        });
+
+        Object.keys(ngTranslations).forEach(key => {
+          if (ngTranslations[key] === "") {
+            ngTranslations[key] = key;
+          }
+        });
+
         return {
-          ...results[0],
-          ...results[1]
+          ...classicTranslations,
+          ...ngTranslations
         };
       })
     );
@@ -41,17 +69,4 @@ export class LanguageLoader extends TranslatePoHttpLoader implements TranslateLo
 
     return mapped;
   }
-
-  private _classicTranslations$ = (lang: string) =>
-    this._jsonApi.getBackendConfig$().pipe(
-      switchMap(backendConfig =>
-        this._http
-          .get(`${environment.classicApiUrl}/json-api/i18n/messages/${lang}/?version=${backendConfig.i18nHash}`, {
-            responseType: "text"
-          })
-          .pipe(map((contents: string) => this.parse(contents)))
-      )
-    );
-
-  private _ngTranslations$ = (lang: string) => this._http.get(`/assets/i18n/${lang}.json?version=${VERSION}`);
 }
