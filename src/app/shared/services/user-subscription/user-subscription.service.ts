b055599d3deed253532ce7482037c4d5338932a7
Salvatore Iovene <salvatore@iovene.com>
2020-11-27 21:36:38 +0100

    (subscriptions) User subscription that are not expired and not active should be valid (#411)

--- src/app/shared/services/user-subscription/user-subscription.service.ts
+++ src/app/shared/services/user-subscription/user-subscription.service.ts
@@ -35,9 +35,7 @@ export class UserSubscriptionService extends BaseService implements UserSubscrip
             appContext.currentUserSubscriptions.filter(userSubscription => {
               const expiration = new Date(userSubscription.expires);
               expiration.setHours(23, 59, 59, 999);
-              return (
-                userSubscription.subscription === subscription.id && userSubscription.active && expiration >= new Date()
-              );
+              return userSubscription.subscription === subscription.id && expiration >= new Date();
             }).length > 0
           ) {
             return true;
