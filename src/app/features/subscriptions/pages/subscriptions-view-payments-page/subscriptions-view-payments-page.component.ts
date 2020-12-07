ae9d309f35d5c2fc58e17f5d3921ed6d1eff1373
Salvatore Iovene <salvatore@iovene.com>
2020-12-07 23:04:46 +0100

    (payments) Add note about CHF on payments page

--- src/app/features/subscriptions/pages/subscriptions-view-payments-page/subscriptions-view-payments-page.component.ts
+++ src/app/features/subscriptions/pages/subscriptions-view-payments-page/subscriptions-view-payments-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
 import { CommonApiService } from "@shared/services/api/classic/common/common-api.service";
 
 @Component({
@@ -6,10 +7,15 @@ import { CommonApiService } from "@shared/services/api/classic/common/common-api
   templateUrl: "./subscriptions-view-payments-page.component.html",
   styleUrls: ["./subscriptions-view-payments-page.component.scss"]
 })
-export class SubscriptionsViewPaymentsPageComponent implements OnInit {
+export class SubscriptionsViewPaymentsPageComponent {
   payments$ = this.commonApiService.getPayments();
 
-  constructor(public readonly commonApiService: CommonApiService) {}
+  constructor(public readonly commonApiService: CommonApiService, public readonly translate: TranslateService) {}
 
-  ngOnInit(): void {}
+  get currencyHelpTooltipMessage(): string {
+    return this.translate.instant(
+      "The price is shown in Swiss Francs even though you might have paid the equivalent amount in your " +
+        "country's currency."
+    );
+  }
 }
