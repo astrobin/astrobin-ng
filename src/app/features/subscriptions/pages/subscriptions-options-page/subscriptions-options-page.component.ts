4b270bf878f0035f195b6e6b72767abdebe75126
Salvatore Iovene <salvatore@iovene.com>
2020-12-12 23:44:55 +0100

    (subscriptions) Display all AstroBin bank accounts (#448)

--- src/app/features/subscriptions/pages/subscriptions-options-page/subscriptions-options-page.component.ts
+++ src/app/features/subscriptions/pages/subscriptions-options-page/subscriptions-options-page.component.ts
@@ -1,27 +1,35 @@
 import { Component, OnInit } from "@angular/core";
 import { PayableProductInterface } from "@features/subscriptions/interfaces/payable-product.interface";
 import { SubscriptionsService } from "@features/subscriptions/services/subscriptions.service";
+import { TranslateService } from "@ngx-translate/core";
+import { BaseComponentDirective } from "@shared/components/base-component.directive";
 import { ClassicRoutesService } from "@shared/services/classic-routes.service";
 import { Observable } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 
 @Component({
   selector: "astrobin-subscriptions-options-page",
   templateUrl: "./subscriptions-options-page.component.html",
   styleUrls: ["./subscriptions-options-page.component.scss"]
 })
-export class SubscriptionsOptionsPageComponent implements OnInit {
+export class SubscriptionsOptionsPageComponent extends BaseComponentDirective implements OnInit {
   litePrice$: Observable<number>;
   premiumPrice$: Observable<number>;
   ultimatePrice$: Observable<number>;
 
   constructor(
     public readonly classicRoutesService: ClassicRoutesService,
-    public readonly subscriptionsService: SubscriptionsService
-  ) {}
+    public readonly subscriptionsService: SubscriptionsService,
+    public readonly translate: TranslateService
+  ) {
+    super();
+  }
 
   ngOnInit(): void {
-    this.litePrice$ = this.subscriptionsService.getPrice(PayableProductInterface.LITE);
-    this.premiumPrice$ = this.subscriptionsService.getPrice(PayableProductInterface.PREMIUM);
-    this.ultimatePrice$ = this.subscriptionsService.getPrice(PayableProductInterface.ULTIMATE);
+    this.subscriptionsService.currency$.pipe(takeUntil(this.destroyed$)).subscribe(() => {
+      this.litePrice$ = this.subscriptionsService.getPrice(PayableProductInterface.LITE);
+      this.premiumPrice$ = this.subscriptionsService.getPrice(PayableProductInterface.PREMIUM);
+      this.ultimatePrice$ = this.subscriptionsService.getPrice(PayableProductInterface.ULTIMATE);
+    });
   }
 }
