b219aad0d4f600da36d535ae1820c219089254a1
Salvatore Iovene <salvatore@iovene.com>
2020-12-28 18:16:09 +0100

    (show zoom indicator)

--- src/app/shared/components/misc/fullscreen-image-viewer/fullscreen-image-viewer.component.ts
+++ src/app/shared/components/misc/fullscreen-image-viewer/fullscreen-image-viewer.component.ts
@@ -10,6 +10,7 @@ import { BaseComponentDirective } from "@shared/components/base-component.direct
 import { ImageAlias } from "@shared/enums/image-alias.enum";
 import { PopNotificationsService } from "@shared/services/pop-notifications.service";
 import { WindowRefService } from "@shared/services/window-ref.service";
+import { Coord } from "ngx-image-zoom";
 import { Observable } from "rxjs";
 import { distinctUntilChanged, filter, map, switchMap, tap } from "rxjs/operators";
 
@@ -29,12 +30,17 @@ export class FullscreenImageViewerComponent extends BaseComponentDirective imple
   klass = "d-none";
 
   zoomLensSize: number;
+  showZoomIndicator = false;
 
   hdThumbnail$: Observable<string>;
   realThumbnail$: Observable<string>;
   show$: Observable<boolean>;
 
   private _zoomReadyNotification;
+  private _zoomPosition: Coord;
+  private _zoomScroll = 1;
+  private _zoomIndicatorTimeout: number;
+  private _zoomIndicatorTimeoutDuration = 3000;
 
   constructor(
     public readonly store$: Store<State>,
@@ -129,6 +135,26 @@ export class FullscreenImageViewerComponent extends BaseComponentDirective imple
     this.ngOnInit();
   }
 
+  setZoomPosition(position: Coord) {
+    this._zoomPosition = position;
+    this.showZoomIndicator = true;
+    this._setZoomIndicatorTimeout();
+  }
+
+  getZoomPosition(): Coord {
+    return this._zoomPosition;
+  }
+
+  setZoomScroll(scroll: number) {
+    this._zoomScroll = scroll;
+    this.showZoomIndicator = true;
+    this._setZoomIndicatorTimeout();
+  }
+
+  getZoomScroll(): number {
+    return this._zoomScroll;
+  }
+
   hide(): void {
     if (this._zoomReadyNotification) {
       this.popNotificationsService.clear(this._zoomReadyNotification.id);
@@ -139,5 +165,15 @@ export class FullscreenImageViewerComponent extends BaseComponentDirective imple
 
   private _setZoomLensSize(): void {
     this.zoomLensSize = Math.floor(this.windowRef.nativeWindow.innerWidth / 3);
+  }
+
+  private _setZoomIndicatorTimeout(): void {
+    if (this._zoomIndicatorTimeout) {
+      clearTimeout(this._zoomIndicatorTimeout);
+    }
+
+    this._zoomIndicatorTimeout = setTimeout(() => {
+      this.showZoomIndicator = false;
+    }, this._zoomIndicatorTimeoutDuration);
   }
 }
