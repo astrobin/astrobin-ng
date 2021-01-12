6c189d3efb70b1e6f4788c2d64fa744d66808078
Salvatore Iovene <salvatore@iovene.com>
2021-01-12 16:40:32 +0100

    (uploader) Allow TIF files (#516)

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,7 +1,15 @@
 export class Constants {
   static readonly AUTO_REFRESH_INTERVAL = 30000;
-  static readonly ALLOWED_UPLOAD_EXTENSIONS = [".jpg", ".jpeg", ".png", ".gif"];
-  static readonly ALLOWED_UNCOMPRESSED_SOURCE_UPLOAD_EXTENSIONS = [".xisf", ".fits", ".fit", ".fts", ".tiff", ".psd"];
+  static readonly ALLOWED_UPLOAD_EXTENSIONS = [".jpg", ".jpeg", ".png", ".gif", ".tif", ".tiff"];
+  static readonly ALLOWED_UNCOMPRESSED_SOURCE_UPLOAD_EXTENSIONS = [
+    ".xisf",
+    ".fits",
+    ".fit",
+    ".fts",
+    ".tif",
+    ".tiff",
+    ".psd"
+  ];
   static readonly NO_VALUE = "NO_VALUE";
   static readonly ORIGINAL_REVISION = "0";
   static readonly SUPPORTED_CURRENCIES = ["USD", "EUR", "GBP", "CAD", "AUD", "CHF"];
