17a0d2d6ac28f98e6aaa5d17d147a923c2f0cd13
Salvatore Iovene <salvatore@iovene.com>
2021-03-01 17:53:21 +0100

    (tests) Fix Uploader.PageComponent tests

--- src/app/features/uploader/pages/uploader-page/uploader-page.component.spec.ts
+++ src/app/features/uploader/pages/uploader-page/uploader-page.component.spec.ts
@@ -1,14 +1,24 @@
+import { TestBed } from "@angular/core/testing";
 import { AppModule } from "@app/app.module";
+import { initialState } from "@app/store/state";
 import { UploaderPageComponent } from "@features/uploader/pages/uploader-page/uploader-page.component";
 import { UploaderModule } from "@features/uploader/uploader.module";
+import { MockStore, provideMockStore } from "@ngrx/store/testing";
 import { MockBuilder, MockRender } from "ng-mocks";
 
 describe("Uploader.PageComponent", () => {
   let component: UploaderPageComponent;
+  let store: MockStore;
 
-  beforeEach(() => MockBuilder(UploaderPageComponent, UploaderModule).mock(AppModule));
+  beforeEach(() =>
+    MockBuilder(UploaderPageComponent, UploaderModule)
+      .mock(AppModule)
+      .provide(provideMockStore({ initialState }))
+  );
 
   beforeEach(() => {
+    store = TestBed.inject(MockStore);
+
     const fixture = MockRender(UploaderPageComponent);
     component = fixture.point.componentInstance;
   });
