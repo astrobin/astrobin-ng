7245b58034b4bb265ec73b9908802a5580d93441
Salvatore Iovene <salvatore@iovene.com>
2021-04-08 19:46:04 +0200

    (fix test)

--- src/app/shared/components/auth/login-form/login-form.component.spec.ts
+++ src/app/shared/components/auth/login-form/login-form.component.spec.ts
@@ -1,8 +1,8 @@
-import { TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { ReactiveFormsModule } from "@angular/forms";
 import { AppModule } from "@app/app.module";
 import { AppGenerator } from "@app/store/generators/app.generator";
-import { appStateEffects, appStateReducers, State } from "@app/store/state";
+import { appStateEffects, appStateReducers, initialState, State } from "@app/store/state";
 import { AuthGenerator } from "@features/account/store/auth.generator";
 import { EffectsModule } from "@ngrx/effects";
 import { StoreModule } from "@ngrx/store";
@@ -13,14 +13,9 @@ import { LoginFormComponent } from "./login-form.component";
 describe("LoginFormComponent", () => {
   let component: LoginFormComponent;
   let store: MockStore;
-  const initialState: State = {
-    app: AppGenerator.default(),
-    auth: AuthGenerator.default()
-  };
 
   beforeEach(() =>
     MockBuilder(LoginFormComponent, AppModule)
-      .keep(ReactiveFormsModule)
       .keep(StoreModule.forRoot(appStateReducers))
       .keep(EffectsModule.forRoot(appStateEffects))
       .provide(provideMockStore({ initialState }))
