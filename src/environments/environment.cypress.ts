5f9dac98efb6eb2e09908b2bd127e0965fc0fc96
Salvatore Iovene <salvatore@iovene.com>
2021-07-17 23:29:16 +0300

    (images) Use rich text editor for image description field (#859)

--- src/environments/environment.cypress.ts
+++ src/environments/environment.cypress.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   classicBaseUrl: "http://localhost:8083",
-  classicApiUrl: "http://localhost:8083"
+  classicApiUrl: "http://localhost:8083",
+  cdnUrl: "http://localhost:8083"
 };
