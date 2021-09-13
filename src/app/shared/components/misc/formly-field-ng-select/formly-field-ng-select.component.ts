ba6d9975dfb5c18816c2ff47f9781aefca4162a6
Salvatore Iovene <salvatore@iovene.com>
2021-09-13 14:34:48 +0200

    (add loading indicator to ng-select)

--- src/app/shared/components/misc/formly-field-ng-select/formly-field-ng-select.component.ts
+++ src/app/shared/components/misc/formly-field-ng-select/formly-field-ng-select.component.ts
@@ -3,7 +3,7 @@ import { FieldType } from "@ngx-formly/core";
 import { TranslateService } from "@ngx-translate/core";
 import { UtilsService } from "@shared/services/utils/utils.service";
 import { isObservable, Observable, Subject, Subscription } from "rxjs";
-import { debounceTime, distinctUntilChanged, takeUntil } from "rxjs/operators";
+import { debounceTime, distinctUntilChanged, takeUntil, tap } from "rxjs/operators";
 
 @Component({
   selector: "astrobin-formly-field-ng-select",
@@ -17,11 +17,19 @@ export class FormlyFieldNgSelectComponent extends FieldType implements OnInit, O
 
   input$ = new Subject<string>();
   inputSubscription: Subscription;
+  loading = false;
 
   ngOnInit() {
-    this.inputSubscription = this.input$.pipe(distinctUntilChanged(), debounceTime(500)).subscribe(value => {
-      this.onSearch(value);
-    });
+    this.inputSubscription = this.input$
+      .pipe(
+        distinctUntilChanged(),
+        tap(() => (this.loading = true)),
+        debounceTime(500)
+      )
+      .subscribe(value => {
+        this.onSearch(value);
+        this.loading = false;
+      });
   }
 
   ngOnDestroy() {
