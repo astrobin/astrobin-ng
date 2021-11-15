f12ada9cf66a4276757e48a22a41b807490ba81c
Salvatore Iovene <salvatore@iovene.com>
2021-11-15 10:13:44 +0100

    (revisions) Add support for title field (#1041)

--- src/app/shared/interfaces/image.interface.ts
+++ src/app/shared/interfaces/image.interface.ts
@@ -176,6 +176,7 @@ export interface ImageRevisionInterface {
   uploaded: string;
   image: ImageInterface["pk"];
   imageFile: string;
+  title: string;
   description: string;
   skipNotifications: boolean;
   label: string;
