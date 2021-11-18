7b70e91872b813c911591c3d845c8e424f0b0b89
Salvatore Iovene <salvatore@iovene.com>
2021-11-18 21:43:16 +0100

    (equipment) Rename migration "commandments" to "guidelines"

---
+++ src/app/features/equipment/components/migration/migration-guidelines/migration-guidelines.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "astrobin-migration-guidelines",
+  templateUrl: "./migration-guidelines.component.html",
+  styleUrls: ["./migration-guidelines.component.scss"]
+})
+export class MigrationGuidelinesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
