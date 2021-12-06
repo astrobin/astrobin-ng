03b5ce6a060b0b01a6d379b40452b9ed5fb51032
Salvatore Iovene <salvatore@iovene.com>
2021-12-06 16:26:26 +0100

    (iotd) Change the queue "Dismiss" button message to explain the new rule (#1061)

--- src/app/features/iotd/components/confirm-dismiss-modal/confirm-dismiss-modal.component.ts
+++ src/app/features/iotd/components/confirm-dismiss-modal/confirm-dismiss-modal.component.ts
@@ -3,6 +3,10 @@ import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
 import { BaseComponentDirective } from "@shared/components/base-component.directive";
 import { Store } from "@ngrx/store";
 import { State } from "@app/store/state";
+import { Observable } from "rxjs";
+import { selectBackendConfig } from "@app/store/selectors/app/app.selectors";
+import { filter, map } from "rxjs/operators";
+import { TranslateService } from "@ngx-translate/core";
 
 export enum ConfirmDismissResult {
   CANCEL,
@@ -15,8 +19,31 @@ export enum ConfirmDismissResult {
 })
 export class ConfirmDismissModalComponent extends BaseComponentDirective {
   ConfirmDismissResult = ConfirmDismissResult;
+  maxDismissals$: Observable<number> = this.store$.select(selectBackendConfig).pipe(
+    filter(backendConfig => !!backendConfig),
+    map(backendConfig => backendConfig.IOTD_MAX_DISMISSALS)
+  );
 
-  constructor(public readonly store$: Store<State>, public readonly modal: NgbActiveModal) {
+  constructor(
+    public readonly store$: Store<State>,
+    public readonly modal: NgbActiveModal,
+    public readonly translateService: TranslateService
+  ) {
     super(store$);
+  }
+
+  get maxDismissalsMessage$(): Observable<string> {
+    return this.maxDismissals$.pipe(
+      map(maxDismissals => {
+        return this.translateService.instant(
+          "When an image is dismissed by {{0}} members of the IOTD/TP staff, it's automatically removed from " +
+            "all queues. Please only perform this action if you want to vote against this image advancing in the " +
+            "process.",
+          {
+            "0": maxDismissals
+          }
+        );
+      })
+    );
   }
 }
