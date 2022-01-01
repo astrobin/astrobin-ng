fe0e6597d16cceb5517e2d4c97cddcd653a033cb
Salvatore Iovene <salvatore@iovene.com>
2022-01-01 12:14:28 +0100

    (images) Add new subject type: noctilucent clouds

--- src/app/features/image/services/image-edit-content-fields.service.ts
+++ src/app/features/image/services/image-edit-content-fields.service.ts
@@ -85,6 +85,7 @@ export class ImageEditContentFieldsService extends BaseService {
           { value: SubjectType.WIDE_FIELD, label: this.translateService.instant("Extremely wide field") },
           { value: SubjectType.STAR_TRAILS, label: this.translateService.instant("Star trails") },
           { value: SubjectType.NORTHERN_LIGHTS, label: this.translateService.instant("Northern lights") },
+          { value: SubjectType.NOCTILUCENT_CLOUDS, label: this.translateService.instant("Noctilucent clouds") },
           { value: SubjectType.GEAR, label: this.translateService.instant("Gear") },
           { value: SubjectType.OTHER, label: this.translateService.instant("Other") }
         ]
