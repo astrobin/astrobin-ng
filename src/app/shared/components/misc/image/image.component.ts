a17d6effc18cb575f744652eb85cd1143a44d8ed
Salvatore Iovene <salvatore@iovene.com>
2022-04-16 15:02:28 +0200

    (iotd) Fix proportions of images in queues if revision has different w/h than original

--- src/app/shared/components/misc/image/image.component.ts
+++ src/app/shared/components/misc/image/image.component.ts
@@ -10,7 +10,7 @@ import {
   SimpleChanges
 } from "@angular/core";
 import { DomSanitizer, SafeUrl } from "@angular/platform-browser";
-import { LoadImage } from "@app/store/actions/image.actions";
+import { LoadImage, LoadImageRevisions } from "@app/store/actions/image.actions";
 import { LoadThumbnail } from "@app/store/actions/thumbnail.actions";
 import { selectImage } from "@app/store/selectors/app/image.selectors";
 import { selectThumbnail } from "@app/store/selectors/app/thumbnail.selectors";
@@ -18,13 +18,14 @@ import { State } from "@app/store/state";
 import { Store } from "@ngrx/store";
 import { BaseComponentDirective } from "@shared/components/base-component.directive";
 import { ImageAlias } from "@shared/enums/image-alias.enum";
-import { ImageInterface } from "@shared/interfaces/image.interface";
+import { ImageInterface, ImageRevisionInterface } from "@shared/interfaces/image.interface";
 import { ImageApiService } from "@shared/services/api/classic/images/image/image-api.service";
 import { ImageService } from "@shared/services/image/image.service";
 import { UtilsService } from "@shared/services/utils/utils.service";
 import { WindowRefService } from "@shared/services/window-ref.service";
 import { debounceTime, distinctUntilChanged, filter, map, switchMap, take, takeUntil } from "rxjs/operators";
-import { fromEvent } from "rxjs";
+import { fromEvent, Observable, of } from "rxjs";
+import { selectImageRevisionsForImage } from "@app/store/selectors/app/image-revision.selectors";
 
 @Component({
   selector: "astrobin-image",
@@ -106,19 +107,43 @@ export class ImageComponent extends BaseComponentDirective implements OnInit, On
       .select(selectImage, this.id)
       .pipe(
         filter(image => !!image),
-        take(1)
+        take(1),
+        switchMap(image => this._loadRevision(image).pipe(map(revision => ({ image, revision }))))
       )
-      .subscribe(image => {
+      .subscribe(({ image, revision }) => {
         this.image = image;
-        this._setWidthAndHeight(image.w, image.h);
+        const w = !!revision ? revision.w : image.w;
+        const h = !!revision ? revision.h : image.h;
+        this._setWidthAndHeight(w, h);
         this._loadThumbnail();
       });
 
     this.store$.dispatch(new LoadImage(this.id));
+    this.store$.dispatch(new LoadImageRevisions({ imageId: this.id }));
   }
 
   onLoad(event) {
     this.loaded.emit();
+  }
+
+  private _loadRevision(image: ImageInterface): Observable<ImageRevisionInterface> {
+    if (this.revision === "0") {
+      return of(null);
+    }
+
+    return this.store$.select(selectImageRevisionsForImage, image.pk).pipe(
+      map(imageRevisions => {
+        const matchingRevisions = imageRevisions.filter(
+          imageRevision => (imageRevision.isFinal && this.revision === "final") || imageRevision.label === this.revision
+        );
+
+        if (matchingRevisions.length > 0) {
+          return imageRevisions[0];
+        }
+
+        return null;
+      })
+    );
   }
 
   private _loadThumbnail() {
