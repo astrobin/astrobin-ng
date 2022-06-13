3e4ee09f0924dce885be71bf87813fc6b08c2b60
Salvatore Iovene <salvatore@iovene.com>
2022-06-13 20:32:43 +0200

    (equipment) Add loading indicator when loading users

--- src/app/features/equipment/components/explorer/users-using-item/users-using-item.component.ts
+++ src/app/features/equipment/components/explorer/users-using-item/users-using-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges } from "@angular/core";
+import { Component, Input, OnChanges, OnInit } from "@angular/core";
 import { BaseComponentDirective } from "@shared/components/base-component.directive";
 import { Store } from "@ngrx/store";
 import { State } from "@app/store/state";
@@ -8,13 +8,14 @@ import { GetUsers } from "@features/equipment/store/equipment.actions";
 import { UserInterface } from "@shared/interfaces/user.interface";
 import { selectUsersUsingEquipmentItem } from "@features/equipment/store/equipment.selectors";
 import { filter, takeUntil } from "rxjs/operators";
+import { LoadingService } from "@shared/services/loading.service";
 
 @Component({
   selector: "astrobin-users-using-equipment-item",
   templateUrl: "./users-using-item.component.html",
   styleUrls: ["../objects-using-item.component.scss"]
 })
-export class UsersUsingItemComponent extends BaseComponentDirective implements OnChanges {
+export class UsersUsingItemComponent extends BaseComponentDirective implements OnInit, OnChanges {
   @Input()
   itemType: EquipmentItemType;
 
@@ -23,25 +24,29 @@ export class UsersUsingItemComponent extends BaseComponentDirective implements O
 
   users: UserInterface[];
 
-  constructor(public readonly store$: Store<State>, public readonly equipmentItemService: EquipmentItemService) {
+  constructor(
+    public readonly store$: Store<State>,
+    public readonly equipmentItemService: EquipmentItemService,
+    public readonly loadingService: LoadingService
+  ) {
     super(store$);
+  }
+
+  ngOnInit(): void {
+    const data = { itemType: this.itemType, itemId: this.itemId };
+
+    this.store$
+      .select(selectUsersUsingEquipmentItem, data)
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe(usersUsingEquipmentItem => {
+        this.users = usersUsingEquipmentItem ? usersUsingEquipmentItem.users : [];
+        this.loadingService.setLoading(false);
+      });
   }
 
   ngOnChanges(): void {
     const data = { itemType: this.itemType, itemId: this.itemId };
-
-    this.users = undefined;
-
-    this.store$
-      .select(selectUsersUsingEquipmentItem, data)
-      .pipe(
-        takeUntil(this.destroyed$),
-        filter(usersUsingEquipmentItem => !!usersUsingEquipmentItem)
-      )
-      .subscribe(usersUsingEquipemtnItem => {
-        this.users = usersUsingEquipemtnItem.users;
-      });
-
+    this.loadingService.setLoading(true);
     this.store$.dispatch(new GetUsers(data));
   }
 }
