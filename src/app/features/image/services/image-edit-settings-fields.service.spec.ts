9b2af647487cbf8c85b8b29529b5374d4097c7dc
Salvatore Iovene <salvatore@iovene.com>
2022-06-29 11:19:19 +0200

    (fix test)

--- src/app/features/image/services/image-edit-settings-fields.service.spec.ts
+++ src/app/features/image/services/image-edit-settings-fields.service.spec.ts
@@ -3,12 +3,14 @@ import { TestBed } from "@angular/core/testing";
 import { ImageEditSettingsFieldsService } from "./image-edit-settings-fields.service";
 import { MockBuilder } from "ng-mocks";
 import { ImageModule } from "@features/image/image.module";
+import { provideMockStore } from "@ngrx/store/testing";
+import { initialState } from "@app/store/state";
 
 describe("ImageEditSettingsFieldsService", () => {
   let service: ImageEditSettingsFieldsService;
 
   beforeEach(async () => {
-    await MockBuilder(ImageEditSettingsFieldsService, ImageModule);
+    await MockBuilder(ImageEditSettingsFieldsService, ImageModule).provide([provideMockStore({ initialState })]);
     service = TestBed.inject(ImageEditSettingsFieldsService);
   });
 
