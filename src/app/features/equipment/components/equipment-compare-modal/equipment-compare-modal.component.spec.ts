59d4ad6495fe9ed8c1e6687e0de05c860263ee6e
Salvatore Iovene <salvatore@iovene.com>
2022-07-05 17:52:07 +0200

    (fix tests)

--- src/app/features/equipment/components/equipment-compare-modal/equipment-compare-modal.component.spec.ts
+++ src/app/features/equipment/components/equipment-compare-modal/equipment-compare-modal.component.spec.ts
@@ -5,13 +5,19 @@ import { MockBuilder } from "ng-mocks";
 import { AppModule } from "@app/app.module";
 import { provideMockStore } from "@ngrx/store/testing";
 import { initialState } from "@app/store/state";
+import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+import { CompareService } from "@features/equipment/services/compare.service";
 
 describe("EquipmentCompareModalComponent", () => {
   let component: EquipmentCompareModalComponent;
   let fixture: ComponentFixture<EquipmentCompareModalComponent>;
 
   beforeEach(async () => {
-    await MockBuilder(EquipmentCompareModalComponent, AppModule).provide([provideMockStore({ initialState })]);
+    await MockBuilder(EquipmentCompareModalComponent, AppModule).provide([
+      provideMockStore({ initialState }),
+      NgbActiveModal,
+      CompareService
+    ]);
   });
 
   beforeEach(() => {
--- src/app/features/equipment/components/equipment-compare/equipment-compare.component.spec.ts
+++ src/app/features/equipment/components/equipment-compare/equipment-compare.component.spec.ts
@@ -1,15 +1,21 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { EquipmentCompareComponent } from "./equipment-compare.component";
+import { MockBuilder } from "ng-mocks";
+import { AppModule } from "@app/app.module";
+import { provideMockStore } from "@ngrx/store/testing";
+import { initialState } from "@app/store/state";
+import { CompareService } from "@features/equipment/services/compare.service";
 
 describe("CompareComponent", () => {
   let component: EquipmentCompareComponent;
   let fixture: ComponentFixture<EquipmentCompareComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [EquipmentCompareComponent]
-    }).compileComponents();
+    await MockBuilder(EquipmentCompareComponent, AppModule).provide([
+      provideMockStore({ initialState }),
+      CompareService
+    ]);
   });
 
   beforeEach(() => {
