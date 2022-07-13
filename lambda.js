91e4b3057e86dce1a8dca383923f08bbe5319b85
Salvatore Iovene <salvatore@iovene.com>
2022-07-13 09:51:57 +0200

    (binaryMimeTypes)

--- lambda.js
+++ lambda.js
@@ -1,7 +1,28 @@
 const awsServerlessExpress = require("aws-serverless-express");
 const awsServerlessExpressMiddleware = require("aws-serverless-express/middleware");
 const app = require("./dist/frontend/server/main");
-const serverProxy = awsServerlessExpress.createServer(app.server);
+const binaryMimeTypes = [
+  "application/javascript",
+  "application/json",
+  "application/octet-stream",
+  "application/xml",
+  "image/jpeg",
+  "image/png",
+  "image/gif",
+  "text/comma-separated-values",
+  "text/css",
+  "text/html",
+  "text/javascript",
+  "text/plain",
+  "text/text",
+  "text/xml",
+  "image/x-icon",
+  "image/svg+xml",
+  "application/x-font-ttf",
+  "font/ttf",
+  "font/otf"
+];
+const serverProxy = awsServerlessExpress.createServer(app.server, undefined, binaryMimeTypes);
 
 app.server.use(awsServerlessExpressMiddleware.eventContext());
 
