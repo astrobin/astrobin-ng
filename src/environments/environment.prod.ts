e8c02d8a362f02354622368ae1c2a28c652912c9
Salvatore Iovene <salvatore@iovene.com>
2022-07-13 15:42:39 +0200

    (use base64 encoding for tiled backgrounds, update version hash, fix buildspec)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  buildVersion: "20220711.1",
+  buildVersion: "d76e13bc9b1918b2e0d75d6339210588",
   classicBaseUrl: "https://www.astrobin.com",
   classicApiUrl: "https://www.astrobin.com",
   cdnUrl: "https://cdn.astrobin.com"
