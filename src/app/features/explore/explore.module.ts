b57e84e27d41b0dcd8d072e710d805e31a963a20
Salvatore Iovene <salvatore@iovene.com>
2022-07-18 17:45:44 +0200

    (general) Add SSRExclude to speed up certain components that are slow on SSR

--- src/app/features/explore/explore.module.ts
+++ src/app/features/explore/explore.module.ts
@@ -3,9 +3,10 @@ import { ConstellationsPageComponent } from "./pages/constellations-page/constel
 import { SharedModule } from "@shared/shared.module";
 import { RouterModule } from "@angular/router";
 import { routes } from "@features/explore/explore.routing";
+import { SSRExcludeModule } from "ngx-ssr-exclude";
 
 @NgModule({
   declarations: [ConstellationsPageComponent],
-  imports: [RouterModule.forChild(routes), SharedModule]
+  imports: [RouterModule.forChild(routes), SharedModule, SSRExcludeModule]
 })
 export class ExploreModule {}
