e32d84937f91b892c850146240a3f801c07f6365
Salvatore Iovene <salvatore@iovene.com>
2022-07-31 21:41:21 +0200

    (remove dummy test file)

--- src/app/shared/directives/gtag-outbound-click-event/gtag-outbound-click-event.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GtagOutboundClickEventDirective } from "./gtag-outbound-click-event.directive";
-
-describe("EventPreventDefaultDirective", () => {
-  it("should create an instance", () => {
-    const directive = new GtagOutboundClickEventDirective();
-    expect(directive).toBeTruthy();
-  });
-});
