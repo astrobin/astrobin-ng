ebe3e098cb043bab5fb5f843eba92e3dae36fe0d
Salvatore Iovene <salvatore@iovene.com>
2022-07-31 21:21:04 +0200

    (fix lint)

--- src/app/features/equipment/equipment.module.ts
+++ src/app/features/equipment/equipment.module.ts
@@ -32,7 +32,7 @@ import { UnapproveItemModalComponent } from "@features/equipment/components/unap
 import { ContributorsPageComponent } from "./pages/contributors-page/contributors-page.component";
 import { EquipmentCompareComponent } from "./components/equipment-compare/equipment-compare.component";
 import { EquipmentCompareModalComponent } from "./components/equipment-compare-modal/equipment-compare-modal.component";
-import { EquipmentListingsComponent } from './components/equipment-listings/equipment-listings.component';
+import { EquipmentListingsComponent } from "./components/equipment-listings/equipment-listings.component";
 
 @NgModule({
   declarations: [
