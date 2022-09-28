c3be408e66a33741b2996907f2597765f40343a3
Salvatore Iovene <salvatore@iovene.com>
2022-09-28 11:19:30 +0200

    (fix test)

--- src/app/shared/components/equipment/item-browser-by-properties/item-browser-by-properties.component.spec.ts
+++ src/app/shared/components/equipment/item-browser-by-properties/item-browser-by-properties.component.spec.ts
@@ -5,18 +5,32 @@ import { MockBuilder } from "ng-mocks";
 import { AppModule } from "@app/app.module";
 import { provideMockStore } from "@ngrx/store/testing";
 import { initialState } from "@app/store/state";
+import { provideMockActions } from "@ngrx/effects/testing";
+import { of, ReplaySubject } from "rxjs";
 
 describe("ItemBrowserByPropertiesModalComponent", () => {
   let component: ItemBrowserByPropertiesComponent;
   let fixture: ComponentFixture<ItemBrowserByPropertiesComponent>;
 
   beforeEach(async () => {
-    await MockBuilder(ItemBrowserByPropertiesComponent, AppModule).provide(provideMockStore({ initialState }));
+    await MockBuilder(ItemBrowserByPropertiesComponent, AppModule).provide([
+      provideMockStore({ initialState }),
+      provideMockActions(() => new ReplaySubject<any>())
+    ]);
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ItemBrowserByPropertiesComponent);
     component = fixture.componentInstance;
+
+    jest
+      .spyOn(component.equipmentApiService, "findAllEquipmentItems")
+      .mockReturnValue(of({ count: 0, prev: null, next: null, results: [] }));
+
+    jest
+      .spyOn(component.equipmentApiService, "getBrandsByEquipmentType")
+      .mockReturnValue(of({ count: 0, prev: null, next: null, results: [] }));
+
     fixture.detectChanges();
   });
 
