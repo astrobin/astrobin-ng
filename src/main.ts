fbde500408e220d0497ff5e9db13b99731b8c77c
Salvatore Iovene <salvatore@iovene.com>
2022-10-04 10:07:24 +0200

    (general) Remove check for dev mode when initializing Sentry

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode, isDevMode } from "@angular/core";
+import { enableProdMode } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
 import { AppModule } from "@app/app.module";
@@ -10,7 +10,7 @@ Sentry.init({
   dsn: `https://${environment.sentryKeys[0]}@${environment.sentryKeys[1]}.ingest.sentry.io/${environment.sentryKeys[2]}`,
   integrations: [
     new BrowserTracing({
-      tracingOrigins: ["localhost", "https://yourserver.io/api"],
+      tracingOrigins: ["localhost", "https://app.astrobin.com/"],
       routingInstrumentation: Sentry.routingInstrumentation
     })
   ],
@@ -18,7 +18,7 @@ Sentry.init({
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
-  tracesSampleRate: isDevMode() ? 1.0 : 0.1
+  tracesSampleRate: 0.1
 });
 
 if (environment.production) {
