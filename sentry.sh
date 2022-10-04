a39f8f4e018a007dc0a3e829143f5ead49fe108f
Salvatore Iovene <salvatore@iovene.com>
2022-10-04 10:03:17 +0200

    (infra) Place sentry release code in separate script

---
+++ sentry.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -n "$SENTRY_RELEASE" ]; then
+  curl -sL https://sentry.io/get-cli/ | SENTRY_CLI_VERSION="2.6.0" bash
+  sentry-cli login --auth-token ${SENTRY_AUTH_TOKEN}
+  sentry-cli releases new -p $SENTRY_PROJECT $CODEBUILD_RESOLVED_SOURCE_VERSION
+  sentry-cli releases set-commits $CODEBUILD_RESOLVED_SOURCE_VERSION --auto
+  sentry-cli releases files $CODEBUILD_RESOLVED_SOURCE_VERSION upload-sourcemaps dist/frontend/browser/
+  sentry-cli releases finalize $CODEBUILD_RESOLVED_SOURCE_VERSION
+  sentry-cli releases deploys $CODEBUILD_RESOLVED_SOURCE_VERSION new -e $SENTRY_ENVIRONMENT
+fi
