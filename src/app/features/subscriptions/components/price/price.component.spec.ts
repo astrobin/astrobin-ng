635e0f8b38ca7435a51f48fd676823057a10c82c
Salvatore Iovene <salvatore@iovene.com>
2023-06-01 18:02:00 +0200

    fix tests

--- src/app/features/subscriptions/components/price/price.component.spec.ts
+++ src/app/features/subscriptions/components/price/price.component.spec.ts
@@ -1,23 +1,24 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { PriceComponent } from './price.component';
+import { PriceComponent } from "./price.component";
+import { MockBuilder } from "ng-mocks";
+import { AppModule } from "@app/app.module";
+import { provideMockStore } from "@ngrx/store/testing";
+import { initialState } from "@app/store/state";
 
-describe('PriceComponent', () => {
+describe("PriceComponent", () => {
   let component: PriceComponent;
   let fixture: ComponentFixture<PriceComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PriceComponent ]
-    })
-    .compileComponents();
+    await MockBuilder(PriceComponent, AppModule).provide([provideMockStore({ initialState })]);
 
     fixture = TestBed.createComponent(PriceComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
