43edc5258a63062aeb2e64ec82cc842b7c6be51b
Salvatore Iovene <salvatore@iovene.com>
2023-06-02 09:37:16 +0200

    fix cypress

--- cypress/integration/subscriptions/ultimate.js
+++ cypress/integration/subscriptions/ultimate.js
@@ -16,12 +16,29 @@ context("ultimate", () => {
       cy.server();
       cy.setupInitializationRoutes();
 
-      cy.route("get", "**/payments/pricing/ultimate/USD/", {
+      cy.route("get", "**/payments/pricing/available-subscriptions/", {
+        nonAutorenewingSupported: true,
+        subscriptions: [
+          { name: "AstroBin Lite 2020+", displayName: "Lite" },
+          { name: "AstroBin Premium 2020+", displayName: "Premium" },
+          { name: "AstroBin Ultimate 2020+", displayName: "Ultimate" }
+        ]
+      }).as("availableSubscriptions");
+
+      cy.route("get", "**/payments/pricing/ultimate/USD/yearly/", {
         discount: 0,
-        price: 20,
-        fullPrice: 20,
+        price: 60,
+        fullPrice: 60,
         prorateAmount: 0
       }).as("pricing");
+
+      cy.route("get", "**/payments/pricing/ultimate/USD/monthly/", {
+        discount: 0,
+        price: 7.5,
+        fullPrice: 7.5,
+        prorateAmount: 0
+      }).as("pricingMonthly");
+
       cy.route("get", "**/images/image/?user=1", {}).as("userImages");
 
       cy.login();
@@ -39,8 +56,7 @@ context("ultimate", () => {
         "getUserSubscriptions"
       );
       cy.visitPage("/subscriptions/ultimate");
-      cy.get(".already-subscribed.alert").should("not.exist");
-      cy.get(".already-subscribed-higher.alert").should("not.exist");
+      cy.get(".already-subscribed").should("not.exist");
       cy.get(".buy.btn").should("exist");
     });
 
@@ -49,27 +65,24 @@ context("ultimate", () => {
         "getUserSubscriptions"
       );
       cy.visitPage("/subscriptions/ultimate");
-      cy.get(".already-subscribed.alert").should("not.exist");
-      cy.get(".already-subscribed-higher.alert").should("not.exist");
+      cy.get(".already-subscribed").should("not.exist");
       cy.get(".buy.btn").should("exist");
     });
 
-    it("should allow purchase if the user is already on Ultimate", () => {
+    it("should not allow purchase if the user is already on Ultimate", () => {
       cy.route("get", "**/common/usersubscriptions/?user=*", "fixture:api/common/usersubscriptions_1_ultimate.json").as(
         "getUserSubscriptions"
       );
       cy.visitPage("/subscriptions/ultimate");
-      cy.get(".already-subscribed.alert").should("exist");
-      cy.get(".already-subscribed-higher.alert").should("not.exist");
-      cy.get(".buy.btn").should("exist");
+      cy.get(".already-subscribed").should("exist");
+      cy.get(".buy.btn").should("not.exist");
     });
 
     it("should allow purchase if the user is not already subscribed", () => {
       cy.route("get", "**/common/usersubscriptions/?user=*", []).as("getUserSubscriptions");
       cy.visitPage("/subscriptions/ultimate");
-      cy.get(".already-subscribed.alert").should("not.exist");
-      cy.get(".already-subscribed-higher.alert").should("not.exist");
-      cy.get(".price").should("contain", "$20.00");
+      cy.get(".already-subscribed").should("not.exist");
+      cy.get(".price").should("contain", "$60.00");
       cy.get(".buy.btn").should("exist");
     });
 
