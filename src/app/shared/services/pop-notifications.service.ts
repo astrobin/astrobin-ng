bba38526063d1a3df65ba23f776aeeb0f4ca36a0
Salvatore Iovene <salvatore@iovene.com>
2023-07-03 13:07:55 +0200

    (ssr) Do not show notifications during SSR and skip updating filter generated name

--- src/app/shared/services/pop-notifications.service.ts
+++ src/app/shared/services/pop-notifications.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from "@angular/core";
+import { Inject, Injectable, PLATFORM_ID } from "@angular/core";
 import { TranslateService } from "@ngx-translate/core";
 import { BaseService } from "@shared/services/base.service";
 import { LoadingService } from "@shared/services/loading.service";
 import { ActiveToast, IndividualConfig, ToastrService } from "ngx-toastr";
+import { isPlatformServer } from "@angular/common";
 
 export interface ToastButtonInterface {
   id: string;
@@ -21,24 +22,37 @@ export class PopNotificationsService extends BaseService {
   public constructor(
     public loadingService: LoadingService,
     public toastr: ToastrService,
-    public translate: TranslateService
+    public translate: TranslateService,
+    @Inject(PLATFORM_ID) public readonly platformId: Object
   ) {
     super(loadingService);
   }
 
   public success(message: string, title?: string, options?: Partial<ExtendedIndividualConfig>): ActiveToast<any> {
+    if (isPlatformServer(this.platformId)) {
+      return;
+    }
     return this.toastr.success(message, title ? title : this.translate.instant("Success!"), options);
   }
 
   public info(message: string, title?: string, options?: Partial<ExtendedIndividualConfig>): ActiveToast<any> {
+    if (isPlatformServer(this.platformId)) {
+      return;
+    }
     return this.toastr.info(message, title ? title : this.translate.instant("Info"), options);
   }
 
   public warning(message: string, title?: string, options?: Partial<ExtendedIndividualConfig>): ActiveToast<any> {
+    if (isPlatformServer(this.platformId)) {
+      return;
+    }
     return this.toastr.warning(message, title ? title : this.translate.instant("Warning!"), options);
   }
 
   public error(message: string, title?: string, options?: Partial<ExtendedIndividualConfig>): ActiveToast<any> {
+    if (isPlatformServer(this.platformId)) {
+      return;
+    }
     return this.toastr.error(message, title ? title : this.translate.instant("Error!"), options);
   }
 
