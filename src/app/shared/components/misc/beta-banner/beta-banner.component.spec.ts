52707c814b970679d8fb51af4d8b24ff062c704a
Salvatore Iovene <salvatore@iovene.com>
2023-07-06 00:30:09 +0200

    (fix test)

--- src/app/shared/components/misc/beta-banner/beta-banner.component.spec.ts
+++ src/app/shared/components/misc/beta-banner/beta-banner.component.spec.ts
@@ -1,23 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { BetaBannerComponent } from './beta-banner.component';
+import { BetaBannerComponent } from "./beta-banner.component";
+import { MockBuilder } from "ng-mocks";
+import { AppModule } from "@app/app.module";
+import { initialState } from "@app/store/state";
+import { provideMockStore } from "@ngrx/store/testing";
+import { WindowRefService } from "@shared/services/window-ref.service";
 
-describe('BetaBannerComponent', () => {
+describe("BetaBannerComponent", () => {
   let component: BetaBannerComponent;
   let fixture: ComponentFixture<BetaBannerComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ BetaBannerComponent ]
-    })
-    .compileComponents();
+    await MockBuilder(BetaBannerComponent, AppModule).provide([WindowRefService, provideMockStore({ initialState })]);
 
     fixture = TestBed.createComponent(BetaBannerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
