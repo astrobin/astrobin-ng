c97fc4500b94ff7a1a5bbd467e26dd6e6dd57b09
Salvatore Iovene <salvatore@iovene.com>
2023-11-17 17:58:38 +0100

    (images/editor) Add support for Astronomy Acres in the list of remote hosting facilities

--- src/app/shared/interfaces/image.interface.ts
+++ src/app/shared/interfaces/image.interface.ts
@@ -75,6 +75,7 @@ export enum RemoteSource {
   ALNI = "Alnitak Remote Observatories",
   AC = "AstroCamp",
   AHK = "Astro Hostel Krasnodar",
+  ACRES = "Astronomy Acres",
   AOWA = "Astro Observatories Western Australia",
   ATLA = "Atlaskies Observatory",
   CS = "ChileScope",
