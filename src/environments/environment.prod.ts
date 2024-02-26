b10c4a8bc893529d2fab77a44dc4aac3be9e159d
Salvatore Iovene <salvatore@iovene.com>
2024-02-26 22:54:41 +0100

    (infra) Temporarily (or permanently?) disable API proxying because I'm seeing lots of disconnects

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   buildVersion: "__BUILD_VERSION__",
   classicBaseUrl: "https://www.astrobin.com",
-  classicApiUrl: "https://app.astrobin.com",  // Proxied by node (see: server.ts)
+  classicApiUrl: "https://www.astrobin.com",
   cdnUrl: "https://cdn.astrobin.com",
   sentryKeys: ["8284df973c49473baa2c08d8212fcde6", "o4503907479519232", "4503907484237824"]
 };
