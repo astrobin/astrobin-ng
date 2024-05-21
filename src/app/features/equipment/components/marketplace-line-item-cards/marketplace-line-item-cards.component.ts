60bbec031646e679c00ba4733890cc4ece0d7557
Salvatore Iovene <salvatore@iovene.com>
2024-05-21 22:07:23 +0200

    (add link to create listing if there's nothing on the list of listings)

--- src/app/features/equipment/components/marketplace-line-item-cards/marketplace-line-item-cards.component.ts
+++ src/app/features/equipment/components/marketplace-line-item-cards/marketplace-line-item-cards.component.ts
@@ -3,6 +3,7 @@ import { BaseComponentDirective } from "@shared/components/base-component.direct
 import { State } from "@app/store/state";
 import { Store } from "@ngrx/store";
 import { MarketplaceListingInterface } from "@features/equipment/types/marketplace-listing.interface";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: "astrobin-marketplace-line-item-cards",
@@ -13,7 +14,7 @@ export class MarketplaceLineItemCardsComponent extends BaseComponentDirective im
   @Input()
   listings: MarketplaceListingInterface[];
 
-  constructor(public readonly store$: Store<State>) {
+  constructor(public readonly store$: Store<State>, public readonly translateService: TranslateService) {
     super(store$);
   }
 
