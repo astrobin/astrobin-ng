b80601faf0994366495472aca17b2dcd19c8d126
Salvatore Iovene <salvatore@iovene.com>
2024-06-07 20:46:24 +0200

    (fix test)

--- src/app/shared/components/misc/country-selection-modal/country-selection-modal.component.spec.ts
+++ src/app/shared/components/misc/country-selection-modal/country-selection-modal.component.spec.ts
@@ -5,6 +5,7 @@ import { MockBuilder } from "ng-mocks";
 import { AppModule } from "@app/app.module";
 import { provideMockStore } from "@ngrx/store/testing";
 import { initialState } from "@app/store/state";
+import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
 
 describe("CountrySelectionModalComponent", () => {
   let component: CountrySelectionModalComponent;
@@ -12,11 +13,18 @@ describe("CountrySelectionModalComponent", () => {
 
   beforeEach(async () => {
     await MockBuilder(CountrySelectionModalComponent, AppModule).provide([
-      provideMockStore({ initialState })
+      provideMockStore({ initialState }),
+      NgbActiveModal
     ]);
 
     fixture = TestBed.createComponent(CountrySelectionModalComponent);
     component = fixture.componentInstance;
+
+    jest.spyOn(component.translateService, "currentLang", "get").mockReturnValue("en");
+    jest.spyOn(component.countryService, "getCountries").mockReturnValue([
+      { code: "US", name: "United States" },
+    ]);
+
     fixture.detectChanges();
   });
 
