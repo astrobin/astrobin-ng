fbe4cece18f226c97420e371408c37d81801ba88
Salvatore Iovene <salvatore@iovene.com>
2024-08-09 13:06:46 +0200

    (infra) Fix failing test

--- src/app/features/search/components/filters/load-save-search-modal/load-save-search-modal.component.spec.ts
+++ src/app/features/search/components/filters/load-save-search-modal/load-save-search-modal.component.spec.ts
@@ -1,19 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoadSaveSearchModalComponent } from './load-save-search-modal.component';
+import { MockBuilder } from "ng-mocks";
+import { AppModule } from "@app/app.module";
+import { provideMockStore } from "@ngrx/store/testing";
+import { initialMainState } from "@app/store/state";
+import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+import { of } from "rxjs";
 
 describe('LoadSaveSearchModalComponent', () => {
   let component: LoadSaveSearchModalComponent;
   let fixture: ComponentFixture<LoadSaveSearchModalComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LoadSaveSearchModalComponent ]
-    })
-    .compileComponents();
+    await MockBuilder(LoadSaveSearchModalComponent, AppModule).provide([
+      provideMockStore({ initialState: initialMainState }),
+      NgbActiveModal
+    ]);
 
     fixture = TestBed.createComponent(LoadSaveSearchModalComponent);
     component = fixture.componentInstance;
+
+    jest.spyOn(component.savedSearchApiService, 'load').mockReturnValue(of([]));
+
     fixture.detectChanges();
   });
 
