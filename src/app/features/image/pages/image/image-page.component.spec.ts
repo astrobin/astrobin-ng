01da7c21b33f45ae976a73bcd257748f6b5d6d5e
Salvatore Iovene <salvatore@iovene.com>
2024-08-20 23:30:41 +0200

    (fix test)

--- src/app/features/image/pages/image/image-page.component.spec.ts
+++ src/app/features/image/pages/image/image-page.component.spec.ts
@@ -26,6 +26,9 @@ describe("ImagePageComponent", () => {
               image
             }
           },
+          data: of({
+            image
+          }),
           fragment: of("1")
         }
       }
