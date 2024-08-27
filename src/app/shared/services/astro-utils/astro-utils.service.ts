16c39a6e3cb9533b85f64157fca54912ae7336dc
Salvatore Iovene <salvatore@iovene.com>
2024-08-27 19:56:46 +0200

    (search) Limit number of decimals in display of coordinates

--- src/app/shared/services/astro-utils/astro-utils.service.ts
+++ src/app/shared/services/astro-utils/astro-utils.service.ts
@@ -7,12 +7,12 @@ export class AstroUtilsService {
   formatRa(value: number): string {
     const hours = Math.floor(value / 60);
     const minutes = value % 60;
-    return `${hours}h ${minutes}m`;
+    return `${hours}h ${minutes.toFixed(2).replace(".00", "")}m`;
   }
 
   formatDec(value: number): string {
     const degrees = Math.floor(value);
     const minutes = Math.abs(value - degrees) * 60;
-    return `${degrees}° ${Math.trunc(minutes)}'`;
+    return `${degrees}° ${minutes.toFixed(2).replace(".00", "")}'`;
   }
 }
