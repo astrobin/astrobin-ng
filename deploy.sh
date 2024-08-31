36644d23b1ab3fbec9e69b903ef6418fab3e8a4a
Salvatore Iovene <salvatore@iovene.com>
2024-08-31 14:28:13 +0200

    (infra) Add debugging messages to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,23 @@
 #!/bin/bash -ex
 
+aws sts get-caller-identity
+
+echo "Starting deploy script..."
+
 AUTOSCALING_GROUP_NAME=$1
 SLEEP_SECONDS=$2
 
+echo "Describing Auto Scaling group: $AUTOSCALING_GROUP_NAME"
+
 AUTOSCALING_GROUP_DESCRIPTION=$(aws autoscaling describe-auto-scaling-groups \
     --auto-scaling-group-names $AUTOSCALING_GROUP_NAME)
+echo "Auto Scaling Group Description: $AUTOSCALING_GROUP_DESCRIPTION"
+
 CURRENT_MIN_SIZE=$(echo "$AUTOSCALING_GROUP_DESCRIPTION" | jq -r ".AutoScalingGroups[0].MinSize")
 CURRENT_MAX_SIZE=$(echo "$AUTOSCALING_GROUP_DESCRIPTION" | jq -r ".AutoScalingGroups[0].MaxSize")
 CURRENT_DESIRED_CAPACITY=$(echo "$AUTOSCALING_GROUP_DESCRIPTION" | jq -r ".AutoScalingGroups[0].DesiredCapacity")
+
+echo "Current Min Size: $CURRENT_MIN_SIZE, Max Size: $CURRENT_MAX_SIZE, Desired Capacity: $CURRENT_DESIRED_CAPACITY"
 
 aws autoscaling update-auto-scaling-group \
     --auto-scaling-group-name ${AUTOSCALING_GROUP_NAME} \
@@ -15,6 +25,7 @@ aws autoscaling update-auto-scaling-group \
     --max-size "$(($CURRENT_MAX_SIZE * 2))" \
     --desired-capacity "$(($CURRENT_DESIRED_CAPACITY * 2))"
 
+echo "Sleeping for $SLEEP_SECONDS seconds..."
 sleep ${SLEEP_SECONDS}
 
 aws autoscaling update-auto-scaling-group \
@@ -22,3 +33,5 @@ aws autoscaling update-auto-scaling-group \
     --min-size ${CURRENT_MIN_SIZE} \
     --max-size ${CURRENT_MAX_SIZE} \
     --desired-capacity ${CURRENT_DESIRED_CAPACITY}
+
+echo "Finished deploy script."
