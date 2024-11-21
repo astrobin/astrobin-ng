60c7a6dd08d4101b4bb74165ecb8d6b9e593ead6
Salvatore Iovene <salvatore@iovene.com>
2024-11-21 10:13:36 +0100

    (galleries) Scroll nav tab to the left only when changing user

--- src/app/features/users/pages/gallery/user-gallery-navigation.component.ts
+++ src/app/features/users/pages/gallery/user-gallery-navigation.component.ts
@@ -305,17 +305,6 @@ export class UserGalleryNavigationComponent extends BaseComponentDirective imple
   }
 
   ngOnInit(): void {
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd),
-      takeUntil(this.destroyed$)
-    ).subscribe(() => {
-      const navTabsElement = this.elementRef.nativeElement.querySelector(".nav-tabs");
-
-      if (navTabsElement) {
-        navTabsElement.scrollLeft = 0;
-      }
-    });
-
     this.route.fragment.pipe(takeUntil(this.destroyed$)).subscribe((fragment: string | null) => {
       this._setActiveTabFromRoute();
     });
@@ -388,6 +377,11 @@ export class UserGalleryNavigationComponent extends BaseComponentDirective imple
 
   ngOnChanges() {
     this._updateSearchModel();
+
+    const navTabsElement = this.elementRef.nativeElement.querySelector(".nav-tabs");
+    if (navTabsElement) {
+      navTabsElement.scrollLeft = 0;
+    }
   }
 
   onTabClick(tab: GalleryNavigationComponent) {
