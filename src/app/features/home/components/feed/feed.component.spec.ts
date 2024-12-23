7dca3013f02728f3b90e05b3792799d454d55702
Salvatore Iovene <salvatore@iovene.com>
2024-12-23 18:15:11 +0100

    (infra) Fix test

--- src/app/features/home/components/feed/feed.component.spec.ts
+++ src/app/features/home/components/feed/feed.component.spec.ts
@@ -8,6 +8,7 @@ import { initialMainState } from "@app/store/state";
 import { Router } from "@angular/router";
 import { EMPTY, of } from "rxjs";
 import { FeedItemGenerator } from "@shared/generators/feed-item.generator";
+import { UtilsService } from "@shared/services/utils/utils.service";
 
 describe('FeedComponent', () => {
   let component: FeedComponent;
@@ -15,6 +16,7 @@ describe('FeedComponent', () => {
 
   beforeEach(async () => {
     await MockBuilder(FeedComponent, AppModule).provide([
+      UtilsService,
       provideMockStore({ initialState: initialMainState }),
       {
         provide: Router,
