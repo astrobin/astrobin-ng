455eba8e865a9a94840ea858242b10de6721dc1b
Salvatore Iovene <salvatore@iovene.com>
2025-02-17 18:47:30 +0100

    (fix zoom on iotd queue pages)

--- src/app/features/iotd/components/base-promotion-entry/base-promotion-entry.component.ts
+++ src/app/features/iotd/components/base-promotion-entry/base-promotion-entry.component.ts
@@ -192,6 +192,10 @@ export abstract class BasePromotionEntryComponent extends BaseComponentDirective
   abstract setExpiration(pk: PromotionImageInterface["pk"]): void;
 
   viewFullscreen(event: MouseEvent | TouchEvent, pk: PromotionImageInterface["pk"]): void {
+    if (event) {
+      event.preventDefault();
+    }
+
     if (!this.image.loading && !!this.image.image && !this.image.image.videoFile) {
       this.store$.dispatch(new ShowFullscreenImage({
         imageId: pk,
