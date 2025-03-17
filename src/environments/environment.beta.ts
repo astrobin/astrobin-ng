c92bd513eb11917d481bb1a186cb53520fd59266
Salvatore Iovene <salvatore@iovene.com>
2025-03-17 17:52:38 +0100

    (images) Add button to reveal Moon to scale (#1741)

--- src/environments/environment.beta.ts
+++ src/environments/environment.beta.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  buildVersion: "1",
-  classicBaseUrl: "https://beta.astrobin.com",
-  classicApiUrl: "https://beta.astrobin.com",
-  cdnUrl: "https://beta-cdn.astrobin.com",
+  buildVersion: "20250316-11",
+  classicBaseUrl: "https://www.astrobin.com",
+  classicApiUrl: "https://www.astrobin.com",
+  cdnUrl: "https://cdn.astrobin.com",
   sentryKeys: ["16bc7458d1ea4101a899a85a85929f7c", "o4503907479519232", "4503907579002880"]
 };
