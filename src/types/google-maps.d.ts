6331abaadd9f0eb104c958a2e97b98c27d040f70
Salvatore Iovene <salvatore@astrobin.com>
2025-04-11 08:48:02 +0200

    (fix npx tsc --noEmit)

---
+++ src/types/google-maps.d.ts
@@ -0,0 +1,89 @@
+declare namespace google {
+  namespace maps {
+    class Geocoder {
+      geocode(
+        request: GeocoderRequest,
+        callback: (results: GeocoderResult[], status: GeocoderStatus) => void
+      ): void;
+    }
+
+    interface GeocoderRequest {
+      address?: string;
+      location?: LatLng;
+      bounds?: LatLngBounds;
+      componentRestrictions?: GeocoderComponentRestrictions;
+      region?: string;
+    }
+
+    interface GeocoderComponentRestrictions {
+      route?: string;
+      locality?: string;
+      administrativeArea?: string;
+      postalCode?: string;
+      country?: string;
+    }
+
+    interface GeocoderResult {
+      address_components: GeocoderAddressComponent[];
+      formatted_address: string;
+      geometry: GeocoderGeometry;
+      partial_match: boolean;
+      place_id: string;
+      plus_code?: {
+        compound_code: string;
+        global_code: string;
+      };
+      types: string[];
+    }
+
+    interface GeocoderAddressComponent {
+      long_name: string;
+      short_name: string;
+      types: string[];
+    }
+
+    interface GeocoderGeometry {
+      bounds?: LatLngBounds;
+      location: LatLng;
+      location_type: GeocoderLocationType;
+      viewport: LatLngBounds;
+    }
+
+    type GeocoderLocationType = 'APPROXIMATE' | 'GEOMETRIC_CENTER' | 'RANGE_INTERPOLATED' | 'ROOFTOP';
+
+    enum GeocoderStatus {
+      ERROR = 'ERROR',
+      INVALID_REQUEST = 'INVALID_REQUEST',
+      OK = 'OK',
+      OVER_QUERY_LIMIT = 'OVER_QUERY_LIMIT',
+      REQUEST_DENIED = 'REQUEST_DENIED',
+      UNKNOWN_ERROR = 'UNKNOWN_ERROR',
+      ZERO_RESULTS = 'ZERO_RESULTS'
+    }
+
+    class LatLng {
+      constructor(lat: number, lng: number, noWrap?: boolean);
+      lat(): number;
+      lng(): number;
+      toString(): string;
+      toUrlValue(precision?: number): string;
+      equals(other: LatLng): boolean;
+    }
+
+    class LatLngBounds {
+      constructor(sw?: LatLng, ne?: LatLng);
+      contains(latLng: LatLng): boolean;
+      equals(other: LatLngBounds): boolean;
+      extend(latLng: LatLng): LatLngBounds;
+      getCenter(): LatLng;
+      getNorthEast(): LatLng;
+      getSouthWest(): LatLng;
+      intersects(other: LatLngBounds): boolean;
+      isEmpty(): boolean;
+      toSpan(): LatLng;
+      toString(): string;
+      toUrlValue(precision?: number): string;
+      union(other: LatLngBounds): LatLngBounds;
+    }
+  }
+}
